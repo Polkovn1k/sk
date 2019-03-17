4239d337e3a95e7bc9f2dfcde10da9e1371de442
Polkovn1k <wa0s@narod.ru>
2019-03-17 14:05:26 +0300

    favorite--6

--- js/script.js
+++ js/script.js
@@ -1,113 +1,120 @@
-// ОБРАБОТЧИК МОДАЛОК
-var modals = {
-//слушаем клик по кнопкам вызывающим оверлэи
-    listenClickForBtnWhichCallOverlay: () => {
-        document.querySelectorAll(".js-overlay-btn").forEach((btnCallingOverlays) => {
-            btnCallingOverlays.addEventListener("click", (event) => {
-               if (btnCallingOverlays && btnCallingOverlays.contains(event.target)) {
-                   event.preventDefault();
-                   modals._actionsWithBtnsAndModals(btnCallingOverlays);
-               }
+
+//---------------------------------------------------------------
+//раскрытие пунктов навигаций в футере
+var footerNav = {
+    catalogButtons: ".js-footer-nav__nav-tog",
+
+    catalogCollapsedContent: ".js-footer-nav__links-list",
+
+    clickListener: (btns) => {
+        document.querySelectorAll(btns).forEach(function(item) {
+            item.addEventListener("click", (event) => {
+                if (!item.classList.contains("active-btn")) {
+                    footerNav.removeAllBtnsActive();
+                    footerNav.removeAllContentsHeight();
+                    item.classList.add("active-btn");
+                    footerNav.openDataToIdContent(item);
+                    return false;
+                }
+                footerNav.removeAllBtnsActive();
+                footerNav.removeAllContentsHeight();
             });
         });
     },
 
-//слушаем клик по кнопке закрывающей все оверлэи
-    closeAllOverlaysBtn: () => {
-        document.querySelectorAll(".js-close-overlays").forEach((btnCloseAllOverlay) => {
-            btnCloseAllOverlay.addEventListener("click", (event) => {
-               modals._hideOverlays();//убираем все оверлэи
-               modals._deleteButtonsStateForAllBtn();//убираем active-btn у всех кнопок
-               modals._toggleHtmlScrollForOverlays("enable");//возвращаем скролл для html/body
-            });
-        });
-    },
-
-//слушаем поворот устройства
-    listenTurnDevice: () => {
-        window.addEventListener("orientationchange", function() {
-            modals._hideOverlays();//убираем все оверлэи
-            modals._deleteButtonsStateForAllBtn();//убираем active-btn у всех кнопок
-            modals._toggleHtmlScrollForOverlays("enable");//возвращаем скролл для html/body
-        });
-    },
-
-//слушаем ресайз вьюпорта
-    listenResizeDevice: () => {
-        window.addEventListener("resize", function() {
-            modals._hideOverlays();//убираем все оверлэи
-            modals._deleteButtonsStateForAllBtn();//убираем active-btn у всех кнопок
-            modals._toggleHtmlScrollForOverlays("enable");//возвращаем скролл для html/body
-        });
-    },
-
-    _actionsWithBtnsAndModals: (clickedBtn) => {
-//если содержит active-btn, то..
-        if (clickedBtn.classList.contains("active-btn")) {
-            modals._hideOverlays();//убираем все оверлэи
-            modals._deleteButtonsStateForAllBtn();//убираем active-btn у всех кнопок
-            modals._toggleHtmlScrollForOverlays("enable");//возвращаем скролл для html/body
-            return false;
-        }
-//если не содержит active-btn, то..
-        modals._hideOverlays();//убираем все оверлэи
-        var foundOverlay = modals._findOverlayByClickedBtn(clickedBtn);//находим нужный нам оверлэй
-        modals._openCurrentOverlay(foundOverlay);//открываем его
-        modals._toggleHtmlScrollForOverlays("disable");//блокируем скролл на html/body
-        modals._deleteButtonsStateForAllBtn();//убираем active-btn у всех кнопок
-        modals._addButtonState(clickedBtn);//и ставим active-btn только на нажатую кнопку
-    },
-
-//убирает active-btn у всех кнопок с классом js-overlay-btn
-    _deleteButtonsStateForAllBtn: () => {
-        document.querySelectorAll(".js-overlay-btn").forEach((element) => {
+    removeAllBtnsActive: () => {
+        document.querySelectorAll(footerNav.catalogButtons).forEach((element) => {
             element.classList.remove("active-btn");
         });
     },
 
-//убираем класс opened у всех оверлеев с классом js-overlay
-    _hideOverlays: () => {
-        document.querySelectorAll(".js-overlay").forEach((element) => {
-            element.classList.remove("opened");
+    removeAllContentsHeight: () => {
+        document.querySelectorAll(footerNav.catalogCollapsedContent).forEach((element) => {
+            element.style.removeProperty("max-height");
         });
     },
 
-//открываем нужный оверлэй в зависимости от нажатой кнопки
-    _openCurrentOverlay: (currentOverlay) => {
-        currentOverlay.classList.add("opened");
-    },
-
-//добавляем класс active-btn к конкретной кнопке
-    _addButtonState: (btn) => {
-        btn.classList.add("active-btn");
-    },
-
-//метод находящий нужный оверлэй в зависимости от нажатии конкретной кнопки
-    _findOverlayByClickedBtn: (clickedBtn) => {
-        var overlayIdForClickedElement = clickedBtn.dataset.overlayId;
-        var overlayForClickedElement = document.getElementById(overlayIdForClickedElement);
-        return overlayForClickedElement;
-    },
-
-//отключаем/включаем скролл на html/body в зависимости от аргумента
-    _toggleHtmlScrollForOverlays(state) {
-        var toggledElems = document.querySelectorAll('html, body');
-        if (state === "disable") {
-            for (var i = 0; i < toggledElems.length; i++) {
-                toggledElems[i].classList.add('scroll-disabled');
-            }
+    openDataToIdContent: (item) => {
+        var findContentElementByID = document.getElementById(item.dataset.findId);
+        if (item.classList.contains("active-btn")) {
+            findContentElementByID.style.maxHeight = findContentElementByID.scrollHeight + "px";
             return false;
         }
-        for (var i = 0; i < toggledElems.length; i++) {
-            toggledElems[i].classList.remove('scroll-disabled');
-        }
-    },
-
-    init: () => {
-        modals.listenClickForBtnWhichCallOverlay();
-        modals.closeAllOverlaysBtn();
-        modals.listenTurnDevice();
-        modals.listenResizeDevice();
     },
 };
-modals.init();
+footerNav.clickListener(footerNav.catalogButtons);
+//тоглер подписки в футера
+var subscription = {
+    subscriptionElement: [".js-subscription-footer-btn", ".js-subscription-footer-input"],
+
+    togleElements: () => {
+        var checkedSubscription = document.querySelector(".js-footer-switch");
+        checkedSubscription.addEventListener("click", (event) => {
+            subscription.subscriptionElement.forEach((element) => {
+                document.querySelector(element).classList.toggle("active");
+            });
+        });
+    },
+};
+subscription.togleElements();
+//модалка входа/регистрации
+var joinModal = {
+    container: document.getElementById("profile-block"),
+    joinBtns: document.querySelectorAll(".js-profile__tab"),
+    joinItems: document.querySelectorAll(".js-profile-item"),
+    btnToDefault: document.querySelectorAll(".js-default-overlay"),
+
+    init: () => {
+        joinModal.joinBtns.forEach(function(item) {
+            item.addEventListener("click", function(event) {
+                joinModal._removeBtnActiveClasses(joinModal.joinBtns);
+                joinModal._removeBlockActiveClasses(joinModal.joinItems);
+                joinModal._openBlock(event.target);
+                joinModal._activeBtn(event.target);
+            });
+        });
+    },
+
+    resetOverlayView: () => {
+        joinModal.btnToDefault.forEach(function(item) {
+            item.addEventListener("click", function(event) {
+                if (!joinModal.container.classList.contains("opened")) {
+                    joinModal._removeBtnActiveClasses(joinModal.joinBtns);
+                    joinModal._removeBlockActiveClasses(joinModal.joinItems);
+                    document.querySelector(".js-profile-join").classList.add("active");
+                    document.querySelector(".js-first-tab").classList.add("active-btn");
+                    return false;
+                }
+            });
+        });
+    },
+
+    _removeBtnActiveClasses: (allBtns) => {
+        allBtns.forEach(function(bItem) {
+            bItem.classList.remove("active-btn");
+        });
+    },
+
+    _removeBlockActiveClasses: (allBlocks) => {
+        allBlocks.forEach(function(jItem) {
+            jItem.classList.remove("active");
+        });
+    },
+
+    _openBlock: (opt) => {
+        var currentBtn = opt.dataset.toClass;
+        for (var i = 0; i < joinModal.joinItems.length; i++) {
+            if (joinModal.joinItems[i].classList.contains(currentBtn)) {
+                joinModal.joinItems[i].classList.add("active");
+            }
+        };
+    },
+
+    _activeBtn: (opt) => {
+        for (var i = 0; i < joinModal.joinBtns.length; i++) {
+            opt.classList.add("active-btn");
+        };
+    },
+};
+joinModal.init();
+joinModal.resetOverlayView();
