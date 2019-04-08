f39e0265d7fb0846656dc5baaac32c18ef55b9fb
Polkovn1k <wa0s@narod.ru>
2019-04-08 07:09:57 +0300

    product-item--32

--- js/module/product-page/product-tab-tog.js
+++ js/module/product-page/product-tab-tog.js
@@ -1,29 +1,40 @@
 //ПЕРЕКЛЮЧАТЕЛЬ ВИДА ТАБОВ НА СТРАНИЦЕ ТОВАРА
 if (document.querySelector(".product-tab-tog")) {
 
-    var gridStyle = {
+    var productTabs = {
 
         listenClick: () => {
-            document.querySelectorAll(".js-sort-view").forEach(function(item) {
-                item.addEventListener("click", function(event) {
-                    gridStyle._switch(item);
+            document.querySelectorAll(".js-product-tab").forEach(function(clickedBtn) {
+                clickedBtn.addEventListener("click", function(event) {
+                    if (clickedBtn.classList.contains(".active")) return
+                    productTabs._removeAllActiveClasses(clickedBtn);
+                    productTabs._addActiveForClickedBtn(clickedBtn);
+                    productTabs._findContainerByBtnAndSetActive(clickedBtn);
                 });
             });
         },
 
-        _switch: (opt) => {
-            document.querySelectorAll(".js-sort-view").forEach(function(elements) {
-                document.getElementById("products-list-container").classList.remove(elements.dataset.toClass);
-                elements.classList.remove("active-btn");
+        _removeAllActiveClasses: (btn) => {
+            document.querySelectorAll(".js-product-tab").forEach((item) => {
+                item.classList.remove("active");
             });
-            document.getElementById("products-list-container").classList.add(opt.dataset.toClass);
-            opt.classList.add("active-btn");
+            document.querySelectorAll(".js-tabs-content").forEach((item) => {
+                item.classList.remove("active");
+            });
+        },
+
+        _addActiveForClickedBtn: (btn) => {
+            btn.classList.add("active");
+        },
+
+        _findContainerByBtnAndSetActive: (btn) => {
+            document.getElementById(btn.dataset.toId).classList.add("active");
         },
 
         init: () => {
-            gridStyle.listenClick();
+            productTabs.listenClick();
         },
     };
-    gridStyle.init();
+    productTabs.init();
 
 }
