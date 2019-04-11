eca6c92ab8deefcb78938bd9fd226d62e17a60f4
Polkovn1k <wa0s@narod.ru>
2019-04-11 08:16:09 +0300

    product-item--58

---
+++ js/module/product-page/product-slider.js
@@ -0,0 +1,32 @@
+//СЛАЙДЕР ТОВАРА - (ДЕТАЛЬНАЯ)
+if (document.querySelector(".product-slider")) {
+
+    $(".js-product-slider-for").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      //lazyLoad: 'ondemand',
+      autoplay: true,
+      arrows: false,
+      fade: false,
+      asNavFor: ".js-product-slider-nav",
+            responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              dots: true,
+              arrows: false
+            }
+          },
+        ]
+    });
+    $(".js-product-slider-nav").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: ".js-product-slider-for",
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      centerPadding: 0
+    });
+
+}
