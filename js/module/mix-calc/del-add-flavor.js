be3cf1fd744c29fc1ca479499845b8a48b2165ed
Polkovn1k <wa0s@narod.ru>
2019-04-18 17:33:46 +0300

    mix-calc--25

---
+++ js/module/mix-calc/del-add-flavor.js
@@ -0,0 +1,37 @@
+//ПЕРЕКЛЮЧАТЕЛЬ ВИДА КАРТОЧЕК: ПЛИТКА/СТРОКА
+if (document.querySelector(".del-add-flavor")) {
+
+    var delAndAddFlavor = {
+
+        btnCollection: document.querySelectorAll(".js-mix-calc-del-flavor"),
+
+        listenRemoveClick: () => {
+            var dynamicCollection = Array.prototype.slice.call(document.querySelectorAll(".js-mix-calc-del-flavor"));
+            dynamicCollection.forEach((btn) => {
+                btn.addEventListener("click", (event) => {
+                    btn.closest(".mix-calc__col").remove();
+                    var btnCollection = document.querySelectorAll(".js-mix-calc-del-flavor");
+                    console.log(document.querySelectorAll(".js-mix-calc-del-flavor").length);
+                    if (btnCollection.length <= 1) {
+                        btnCollection[0].classList.add("hide");
+                        return false;
+                    }
+                })
+            });
+        },
+
+        elementStatusAfterInit: () => {
+            if (delAndAddFlavor.btnCollection.length == 1) {
+                delAndAddFlavor.btnCollection[0].classList.add("hide");
+            }
+        },
+
+        init: () => {
+            delAndAddFlavor.elementStatusAfterInit();
+            delAndAddFlavor.listenRemoveClick();
+        }
+
+    }
+    delAndAddFlavor.init();
+
+}
