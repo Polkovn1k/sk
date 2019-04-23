77dd2ad54ae7d2e6e39f6eca14f309c2db2d8248
Polkovn1k <wa0s@narod.ru>
2019-04-23 14:11:23 +0300

    mix-calc--37

--- js/module/hide-sort-after-click.js
+++ js/module/hide-sort-after-click.js
@@ -7,10 +7,14 @@ if (document.querySelector(".hide-sort")) {
             document.querySelectorAll(".js-product-tab").forEach((item) => {
                 item.addEventListener("click", (event) => {
                     if (item.classList.contains("js-hide-sort")) {
-                        document.querySelector(".sort").classList.add("hide");
+                        document.querySelector(".sort").classList.add("visually-hidden");
+                        document.querySelector(".search-result").classList.add("col-lg-12");
+                        document.querySelector(".js-hide").classList.add("visually-hidden");
                         return false;
                     }
-                    document.querySelector(".sort").classList.remove("hide");
+                    document.querySelector(".sort").classList.remove("visually-hidden");
+                    document.querySelector(".search-result").classList.remove("col-lg-12");
+                    document.querySelector(".js-hide").classList.remove("visually-hidden");
                 });
             });
         },
