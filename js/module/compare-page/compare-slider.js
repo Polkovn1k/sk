353b7914b69d90e7a07e1a28afe15384e913d2d9
Polkovn1k <wa0s@narod.ru>
2019-04-25 16:40:44 +0300

    compare--12

--- js/module/compare-page/compare-slider.js
+++ js/module/compare-page/compare-slider.js
@@ -2,15 +2,15 @@
 if (document.querySelector(".compare-slider")) {
 
     var sliders = document.querySelectorAll(".js-compare-slider");
-    var glideGoodsSliders = [];
+    var glideCompareSliders = [];
     var sliderCollection = function (i, item) {
-        glideGoodsSliders[i] = new Glide(item[i], {
+        glideCompareSliders[i] = new Glide(item[i], {
           gap: 0,
           bound: true,
           rewind: false,
-          perView: 5,
+          perView: 6,
           breakpoints: {
-              1280: {
+              1394: {
                   perView: 4,
               },
               1023: {
@@ -18,8 +18,8 @@ if (document.querySelector(".compare-slider")) {
               },
           }
       });
-        glideGoodsSliders[i].mount();
-        return glideGoodsSliders;
+        glideCompareSliders[i].mount();
+        return glideCompareSliders;
     }
     for (var i = 0; i < sliders.length; i++) {
         sliderCollection(i, sliders);
@@ -38,7 +38,7 @@ if (document.querySelector(".js-main-compare-slider")) {
             window.addEventListener("orientationchange", function() {
                 if (document.documentElement.clientWidth <= compare.START_DESKTOP_WIDTH) return false;
                 //меняем позицию только у 2-го слайдера, т.к. 1-ый скрыт на десктопе
-                glideGoodsSliders[1].update({ startAt: 0 });
+                glideCompareSliders[1].update({ startAt: 0 });
             });
         },
 
@@ -46,7 +46,7 @@ if (document.querySelector(".js-main-compare-slider")) {
             window.addEventListener("resize", function() {
                 if (document.documentElement.clientWidth <= compare.START_DESKTOP_WIDTH) return false;
                 //меняем позицию только у 2-го слайдера, т.к. 1-ый скрыт на десктопе
-                glideGoodsSliders[1].update({ startAt: 0 });
+                glideCompareSliders[1].update({ startAt: 0 });
             });
         },
 
