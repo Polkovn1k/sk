394ee1b312d4f84f7a730a02796024839ca5c97c
Polkovn1k <wa0s@narod.ru>
2019-05-06 16:14:32 +0300

    compare--35

--- js/module/compare-page/compare-slider.js
+++ js/module/compare-page/compare-slider.js
@@ -1,53 +1,79 @@
-//СЛАЙДЕРЫ НА ГЛАВНОЙ СТРАНИЦЕ
+//СЛАЙДЕРЫ СРАВНЕНИЕ
+//БАЗОВЫЕ НАСТРОЙКИ СЛАЙДЕРОВ----------------------------------------------------------------------------------------
 if (document.querySelector(".compare-slider")) {
 
-    var sliders = document.querySelectorAll(".js-compare-slider");
-    var glideCompareSliders = [];
-    var sliderCollection = function (i, item) {
-        glideCompareSliders[i] = new Glide(item[i], {
-          gap: 0,
-          bound: true,
-          rewind: true,
-          perView: 6,
-          breakpoints: {
-              1394: {
-                  perView: 4,
-              },
-              1023: {
-                  perView: 1,
-              },
-          }
-        });
-        glideCompareSliders[i].mount();
-        return glideCompareSliders;
+    var addCompare = new Glide(".js-compare-add-slider", {
+        gap: 0,
+        bound: true,
+        rewind: true,
+        perView: 6,
+        breakpoints: {
+            1394: {
+                perView: 4,
+            },
+            1023: {
+                perView: 1,
+            },
+        }
+    });
+    var addCustomComponent = function (Glide, Components, Events) {
+        return {
+            mount () {
+                addCompare.on("run.before", () => {
+                    console.log(Components.Run.move.direction);
+                });
+            }
+        }
     }
-    for (var i = 0; i < sliders.length; i++) {
-        sliderCollection(i, sliders);
+    addCompare.mount({"createdComponent": addCustomComponent});
+
+    var mainCompare = new Glide(".js-compare-main-slider", {
+        gap: 0,
+        bound: true,
+        rewind: true,
+        startAt: 1,
+        perView: 6,
+        breakpoints: {
+            1394: {
+                perView: 4,
+            },
+            1023: {
+                perView: 1,
+            },
+        }
+    });
+    var mainCustomComponent = function (Glide, Components, Events) {
+        return {
+            mount () {
+                mainCompare.on("run.before", () => {
+                    console.log(Components.Run.move.direction);
+                });
+            }
+        }
     }
 //Второй слайдер всегда начинается со 2-го слайда на мобилках, либо с 1-го на десктопе
     if (document.documentElement.clientWidth < 1024) {
-        glideCompareSliders[1].update({ startAt: 1 });
+        mainCompare.update({ startAt: 1 });
     } else {
-        glideCompareSliders[1].update({ startAt: 0 });
+        mainCompare.update({ startAt: 0 });
     }
+    mainCompare.mount({"createdComponent": mainCustomComponent});
 
-}
-
-//ОБНУЛЯЕМ ПОЗИЦИЮ АКТИВНЫЙХ СЛАЙДЕРОВ, ЕСЛИ ОКНО РЕСАЙЗИТСЯ (ИЛИ МЕНЯЕТСЯ ОРИЕНТАЦИЯ УСТРОЙСТВА)
-if (document.querySelector(".js-main-compare-slider")) {
-
+//ОБЪЕКТ--------------------------------------------------------------------
     var compare = {
 
         START_DESKTOP_WIDTH: 1024,
 
         DESKTOP_XL_SIZE: 1395,
-//Внешний вид при ресайзе или повороте устройства
+
+//Внешний вид при повороте устройства
         listenTurnDevice: () => {
             window.addEventListener("orientationchange", function() {
                 compare._resizeOrTurn();
             });
         },
 
+//Внешний вид при ресайзе окна
         listenResizeDevice: () => {
             window.addEventListener("resize", function() {
                 compare._resizeOrTurn();
@@ -55,35 +81,23 @@ if (document.querySelector(".js-main-compare-slider")) {
         },
 
         _resizeOrTurn: () => {
-            //меняем позицию слайда у второго слайда, в зависимости от вьюпорта
+//Меняем позицию слайда у второго слайда, в зависимости от вьюпорта
             var viewPort = document.documentElement.clientWidth;
             if (viewPort < compare.START_DESKTOP_WIDTH) {
-                glideCompareSliders[1].update({ startAt: 1 });
+                mainCompare.update({ startAt: 1 });
                 return false;
             } else {
-                glideCompareSliders[1].update({ startAt: 0 });
+                mainCompare.update({ startAt: 0 });
             }
-            //устраняем баг, при котором в col-xl при ресайзе показывал только один товар
+//Устраняем баг, при котором в col-xl при ресайзе показывал только один товар
             if (viewPort >= compare.DESKTOP_XL_SIZE) {
-                glideCompareSliders[1].update({ perView: 6 });
+                mainCompare.update({ perView: 6 });
             }
-        },
-//Свойства после инициализации
-        propertyStateAfterInit: () => {
-            document.querySelectorAll(".js-parameter-col").forEach((block) => {
-                block.classList.add("visually-hidden");
-            });
-            compare._propsPosition(glideCompareSliders[0].index);
-            compare._propsPosition(glideCompareSliders[1].index);
         },
 
-        _propsPosition: (currentElement) => {
-            var diffBlock = document.querySelectorAll(".js-parameter-diff-row");
-            diffBlock.forEach((propContainer) => {
-                propContainer.children[currentElement].classList.remove("visually-hidden");
-            });
-        },
-//Свойства при смене слайдов
+
+
+
         /*listenSliderActions: () => {
             for (var i = 0; i < glideCompareSliders.length; i++) {
                 glideCompareSliders[i].on(["mount.before", "run"], function() {
@@ -94,28 +108,22 @@ if (document.querySelector(".js-main-compare-slider")) {
             }
         },*/
 
+        _propsPosition: (currentElement) => {
+            var diffBlock = document.querySelectorAll(".js-parameter-diff-row");
+            diffBlock.forEach((propContainer) => {
+                propContainer.children[currentElement].classList.remove("visually-hidden");
+            });
+        },
+
         _writeCurrentIndex: () => {
             var firstSlider = document.querySelectorAll(".js-compare-slider");
             return firstSlider[0].querySelector(".glide__slide--active");
         },
 
-        _firstSlider: () => {
-            glideCompareSliders[0].on("run.before", () => {
-                compare._writeCurrentIndex().setAttribute("data-index", glideCompareSliders[0].index);
-                console.log(compare._writeCurrentIndex());
-                if (glideCompareSliders[0].index + 1 === glideCompareSliders[1].index) {
-                    glideCompareSliders[0].index++;
-                }
-            });
-            glideCompareSliders[0].mount();
-        },
-
         init: () => {
             compare.listenTurnDevice();
             compare.listenResizeDevice();
-            compare.propertyStateAfterInit();
             //compare.listenSliderActions();
-            compare._firstSlider();
         },
     };
     compare.init();
