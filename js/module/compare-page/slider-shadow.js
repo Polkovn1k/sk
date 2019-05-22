1233e7e13cbada0093175aeba5d2f4cb00c9e711
Polkovn1k <wa0s@narod.ru>
2019-05-22 12:41:17 +0300

    compare-add--14

--- js/module/compare-page/slider-shadow.js
+++ js/module/compare-page/slider-shadow.js
@@ -1,22 +1,27 @@
 //ОТСЛЕЖИВАНИЕ СКРОЛА И ПРОСТАВЛЯЕМ ACTIVE НА КНОПКИ
 if (document.querySelector(".slider-shadow")) {
 
+    let eventHandlerArg = function(event) {
+        console.log("Произошло событие!");
+        var headerBlock = document.querySelector(".js-header");
+        var sliderBlock = document.querySelector(".js-compare-slider-block");
+        var sliderBody = document.querySelector(".js-compare__body");
+        var headerHeight = sliderShadow._getHeaderHeight(".js-header");
+        if ((sliderBlock.getBoundingClientRect().y === headerHeight) || (sliderBlock.getBoundingClientRect().y === 0)) {
+            sliderBlock.classList.add("sticked");
+            headerBlock.classList.add("hide");
+            return false;
+        }
+        sliderBlock.classList.remove("sticked");
+        headerBlock.classList.remove("hide");
+    }
+
     var sliderShadow = {
 
+        scrollEvent: throttle(eventHandlerArg, 100),
+
         listenScroll: () => {
-            window.addEventListener("scroll", (event) => {
-                var headerBlock = document.querySelector(".js-header");
-                var sliderBlock = document.querySelector(".js-compare-slider-block");
-                var sliderBody = document.querySelector(".js-compare__body");
-                var headerHeight = sliderShadow._getHeaderHeight(".js-header");
-                if ((sliderBlock.getBoundingClientRect().y === headerHeight) || (sliderBlock.getBoundingClientRect().y === 0)) {
-                    sliderBlock.classList.add("sticked");
-                    headerBlock.classList.add("hide");
-                    return false;
-                }
-                sliderBlock.classList.remove("sticked");
-                headerBlock.classList.remove("hide");
-            });
+            window.addEventListener("scroll", sliderShadow.scrollEvent);
         },
 
         _getHeaderHeight: (elem) => {
