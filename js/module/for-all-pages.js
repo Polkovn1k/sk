f0e614ca19e8d9d5638fa17a79ed43dd1ffa6751
Polkovn1k <wa0s@narod.ru>
2019-05-28 15:29:46 +0300

    my-version--1

--- js/module/for-all-pages.js
+++ js/module/for-all-pages.js
@@ -397,3 +397,30 @@ function throttle(func, wait, options) {
     return result;
   };
 };
+
+//ACTIVE для кнопок добавить в избранное/сравнение
+if (document.querySelector(".js-active-status")) {
+
+    compareOrFavoriteBtn = {
+
+        listenClick: () => {
+            document.querySelectorAll(".js-active-status").forEach((btn) => {
+                btn.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    compareOrFavoriteBtn.action(btn);
+                });
+            });
+        },
+
+        action: (element) => {
+            element.classList.toggle("added");
+        },
+
+        init: () => {
+            compareOrFavoriteBtn.listenClick();
+        }
+
+    }
+    compareOrFavoriteBtn.init();
+
+}
