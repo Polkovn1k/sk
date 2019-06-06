a460096854a836a3a9b85e7ab89eadec0edff0c5
Polkovn1k <wa0s@narod.ru>
2019-06-06 16:39:26 +0300

    compare-add--35

--- js/module/product-page/product-slider.js
+++ js/module/product-page/product-slider.js
@@ -1,13 +1,11 @@
 //СЛАЙДЕР ТОВАРА - (ДЕТАЛЬНАЯ)
-if (document.querySelector(".product-item-slider")) {
+if (document.querySelector(".js-product-slider-for")) {
 
     var NAV_FOR_SLIDE_QUANTITY = 4;
 
     $(".js-product-slider-for").slick({
       slidesToShow: 1,
       slidesToScroll: 1,
-      //lazyLoad: 'ondemand',
-      //autoplay: true,
       arrows: false,
       fade: false,
       asNavFor: ".js-product-slider-nav",
@@ -26,9 +24,7 @@ if (document.querySelector(".product-item-slider")) {
       slidesToScroll: 1,
       asNavFor: ".js-product-slider-for",
       dots: false,
-      //centerMode: true,
       focusOnSelect: true,
-      //centerPadding: 0
     });
 
 //Если кол-во слайдов ниже 4-х, то отключаем движение нижнего слайдера
